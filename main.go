3e508f5a985b398de6785bafc0cf87c393778fd8
Balint Pataki <pataki.balint@gmx.com>
2022-11-29 23:00:13 +0100

    initial

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"math"
+	"os"
+)
+
+func readCsvFile(path string) [][]string {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal("Unable to read file"+path, err)
+	}
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+path, err)
+	}
+
+	return records
+}
+
+func main() {
+	records := readCsvFile("problems.csv")
+	var correctAnswers float64 = 0
+	var wrongAnswers float64 = 0
+
+	for i := 0; i < len(records); i++ {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println(records[i][0])
+		input, err := reader.ReadString('\n')
+		input = input[:len(input)-1]
+		if err != nil {
+			log.Fatal("Unable to read your input!")
+		}
+
+		if input == records[i][1] {
+			correctAnswers++
+		} else {
+			wrongAnswers++
+		}
+	}
+
+	score := correctAnswers / float64((len(records))) * 100
+
+	fmt.Println("CORRECT ANSWERS: ", correctAnswers)
+	fmt.Println("WRONG ASNWERS: ", wrongAnswers)
+	fmt.Println("YOUR OVERALL SCORE (%): ", math.Round(score))
+}
